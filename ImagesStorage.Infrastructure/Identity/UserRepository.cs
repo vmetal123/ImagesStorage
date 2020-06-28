cde706bb65fe303f0e8acb705e74e6a432eb1b74
vmetal123 <bladi22@yahoo.com>
2020-06-27 21:41:28 -0600

    added methods to get user by username, get user by email, get users

--- ImagesStorage.Infrastructure/Identity/UserRepository.cs
+++ ImagesStorage.Infrastructure/Identity/UserRepository.cs
@@ -1,12 +1,13 @@
 ﻿using AutoMapper;
 using ImagesStorage.Application.Domain.Entities;
 using ImagesStorage.Application.Dto.Repositories;
+using ImagesStorage.Application.Dto.UseCaseRequests;
+using ImagesStorage.Application.Dto.UseCaseResponses;
+using ImagesStorage.Application.Helpers;
 using ImagesStorage.Application.Interfaces.Repositories;
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ImagesStorage.Infrastructure.Identity
@@ -34,6 +35,39 @@ namespace ImagesStorage.Infrastructure.Identity
         public async Task<User> GetUserByIdAsync(string userId)
         {
             return _mapper.Map<User>(await _userManager.FindByIdAsync(userId));
+        }
+
+        public async Task<ImagesStorage.Application.Dto.UseCaseResponses.GetUserByUserNameResponse> GetUserByUsernameAsync(string username)
+        {
+            var user = _mapper.Map<User>(await _userManager.FindByNameAsync(username));
+
+            if(user is null)
+            {
+                return new ImagesStorage.Application.Dto.UseCaseResponses.GetUserByUserNameResponse(new List<string> { "User not found." }, user is null ? false : true, user is null ? "User not found." : null);
+            }
+
+            return new ImagesStorage.Application.Dto.UseCaseResponses.GetUserByUserNameResponse(user, user is null ? false : true, user is null ? "User not found." : null);
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            return _mapper.Map<User>(await _userManager.FindByEmailAsync(email));
+        }
+
+        public async Task<GetUsersResponse> GetUsersAsync(GetUsersRequest queryFilter)
+        {
+            var source = _userManager.Users;
+
+            if(!string.IsNullOrEmpty(queryFilter.SearchTerm))
+            {
+                source = source.Where(x => x.UserName.Contains(queryFilter.SearchTerm) || x.Email.Contains(queryFilter.SearchTerm));
+            }
+
+            var users = _mapper.Map<IEnumerable<User>>(source.AsEnumerable());
+
+            var pagedList = PagedList<User>.Create(users, queryFilter.PageNumber, queryFilter.PageSize);
+
+            return new GetUsersResponse(pagedList, true);
         }
     }
 }
